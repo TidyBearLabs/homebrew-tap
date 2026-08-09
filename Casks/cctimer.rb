1e2bd97613a88eb7b73c73a9679732996ba26f57
Yu-kiKimura <49194887+Yu-kiKimura@users.noreply.github.com>
2026-08-09 18:24:03 +0900

    Update cctimer cask to 0.1.5

--- Casks/cctimer.rb
+++ Casks/cctimer.rb
@@ -1,6 +1,6 @@
 cask "cctimer" do
-  version "0.1.4"
-  sha256 "87b3ba057dd094cc906b63bf67b4fa4708d398c6b8b24520e8da014e655a2942"
+  version "0.1.5"
+  sha256 "4694d1765f20d3208035b3444c81a159047202ebbe660d36e152dcf7d2d231bc"
 
   url "https://github.com/TidyBearLabs/cctimer/releases/download/v#{version}/cctimer-darwin-arm64-#{version}.zip",
       verified: "github.com/TidyBearLabs/cctimer/"
